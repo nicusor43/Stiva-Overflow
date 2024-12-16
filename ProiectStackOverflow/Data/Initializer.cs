fb435b881630cd35291de2ebf4bce28becd4ec17
nicusor <crucerunicusor04@gmail.com>
2024-12-16 17:51:05 +0200

    add Data folder

---
+++ ProiectStackOverflow/Data/Initializer.cs
@@ -0,0 +1,12 @@
+namespace ProiectStackOverflow.Data;
+
+using System.Runtime.CompilerServices;
+
+public static class MyModuleInitializer
+{
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+    }
+}
